5777a56bfc921536903fbca06d5c25ce33f4a280
Saketh Baddam <reddysaketh980@gmail.com>
2023-12-25 16:32:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ model = genai.GenerativeModel(model_name="gemini-pro",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 chat = model.start_chat(history=[])
-displayed_messages = set()
 
 class SpeechChatBot:
     def __init__(self):
